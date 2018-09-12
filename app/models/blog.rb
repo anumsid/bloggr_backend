8ff8dc61f639951e749ed4dfcb1e008108d087eb
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-09-12 19:28:35 -0400

    set up relationship between classes and created validations for user model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ApplicationRecord
+  has_many :comments, through: :users
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-  
+  has_many :blogs
+  has_many :comments
+
+  has_secure_password
+
+  validates :first_name, :last_name, presence: true
+  validates :email, uniqueness: true, allow_nil: false
+  validates :password, length: {minimum: 6, maximum: 18}, on: :create
+  validates :password, presence: true
+
 end
