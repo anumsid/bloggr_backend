26f559165473d44bb69ae8edfbb993de2c5b6dc4
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-09-12 21:19:01 -0400

    set up validations for model blog

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::BlogsController < ApplicationController
+
+
+  # def index
+  #   @blogs = Blog.all
+  #   render json: @blogs
+  # end
+  #
+  # def show
+  #   @blog
+  #   render json: @blog
+  # end
+  #
+  # def create
+  #   @blog = Blog.find_by(title: params['title'])
+  #
+  # end
+
+
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,7 @@
 class Blog < ApplicationRecord
   has_many :comments, through: :users
   belongs_to :user
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
